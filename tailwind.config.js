cbfccd8a1b4a12368188f6d5bf1a9b4ab2a8a448
rat <benyamin11004@gmail.com>
2025-02-09 10:35:53 +0330

    fnish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                vazir: ["vazir", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
